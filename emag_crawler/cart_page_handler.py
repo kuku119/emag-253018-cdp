eb2aff227b0765604f77b18709c801328cc801ce
kkcty <jdycty2002@163.com>
2025-03-19 14:43:23 +0800

    更新代码结构

--- emag_crawler/cart_page_handler.py
+++ emag_crawler/cart_page_handler.py
@@ -4,6 +4,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from scraper_utils.constants.time_constant import MS1000
+from scraper_utils.exceptions.browser_exception import PlaywrightError
+from scraper_utils.utils.browser_util import wait_for_selector
+
+from .utils import logger
+
 if TYPE_CHECKING:
     from playwright.async_api import BrowserContext, Page
 
@@ -30,4 +36,27 @@ async def parse_qty(page: Page):
 
 async def clear_cart(page: Page) -> None:
     """清空购物车"""
-    # TODO
+    logger.info('开始清空购物车')
+
+    # TODO 所有的 Sterge 按钮都点击一边后，是否需要检测购物车是否已经清空？
+
+    sterge_buttons = page.locator('css=button.remove-product').filter(visible=True)
+    for i in range(await sterge_buttons.count(), 0, -1):
+        try:
+            await sterge_buttons.nth(i).click(timeout=MS1000)
+        except PlaywrightError as pe:
+            logger.warning(f'尝试点击第 {i} 个 Sterge 按钮时出错\n{pe}')
+        else:
+            logger.debug(f'Sterge #{i} 成功')
+
+    logger.info('购物车已清空')
+
+
+async def is_empty(page: Page) -> bool:
+    """
+    检测购物车是否为空
+
+    通过检测有无可见的 Sterge 按钮来判断购物车是否为空
+    """
+    visible_sterge_buttons = page.locator('css=button.remove-product').filter(visible=True)
+    return await visible_sterge_buttons.count() == 0
--- emag_crawler/category_page_handler.py
+++ emag_crawler/category_page_handler.py
@@ -2,9 +2,47 @@
 
 from __future__ import annotations
 
+
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    pass
+from .utils import logger
 
-# TODO
+if TYPE_CHECKING:
+    from asyncio.locks import Lock
+
+    from playwright.async_api import BrowserContext, Page, Locator
+    from scraper_utils.constants.time_constant import MS1000
+    from scraper_utils.exceptions.browser_exception import PlaywrightError
+
+
+async def handle_cart_dialog(page: Page, lock: Lock, interval: int = MS1000) -> None:
+    """
+    处理类目页面点击加购按钮后可能出现的弹窗
+
+    当页面还未关闭时，每隔 `interval` 毫秒尝试点击一次加购弹窗的关闭按钮
+
+    """
+    logger.info(f'启动处理加购弹窗任务 "{page.url}"')
+    while page.is_closed() is False:
+        dialog_close_button = page.locator('xpath=//button[@class="close gtm_6046yfqs"]')
+        async with lock:
+            try:
+                await dialog_close_button.click(timeout=interval)
+            except PlaywrightError:
+                pass
+    logger.info(f'检测到页面关闭，处理加购弹窗任务即将关闭')
+
+
+async def parse_first_page(context: BrowserContext, url: str, category: str, lock: Lock):
+    """解析类目页的第 1 页"""
+    # TODO
+
+
+async def parse_other_page(context: BrowserContext, url: str, category: str, lock: Lock):
+    """解析类目页的 2-5 页"""
+    # TODO
+
+
+async def add_to_cart(add_to_cart_button: Locator, lock: Lock):
+    """加购一个产品"""
+    # TODO
